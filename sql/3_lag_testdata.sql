d2cd93b6577ebf342325fa7b24896dae83f12b1e
SeyZ3 <157584578+SeyZ3@users.noreply.github.com>
2024-09-27 12:50:45 +0200

    Delete sql/3_lag_testdata.sql

--- sql/3_lag_testdata.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO person (fornavn, etternavn, telefonnummer)
-VALUES ('Erik', 'Perik', '12345678');
-INSERT INTO person (fornavn, etternavn, telefonnummer)
-VALUES ('Lise', 'Pise', '22334455');
-INSERT INTO person (fornavn, etternavn, telefonnummer)
-VALUES ('Testus', 'Jensen', '11114444');
-INSERT INTO person (fornavn, etternavn, telefonnummer)
-VALUES ('Knut', 'Donald', '31415926');
