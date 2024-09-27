4d6021b9d95b67e155a2aeba79a683c2adeeab11
SeyZ3 <157584578+SeyZ3@users.noreply.github.com>
2024-09-27 12:50:29 +0200

    Delete sql/4_select_data.sql

--- sql/4_select_data.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM person;
-
-SELECT fornavn, telefonnummer FROM person;
-
-SELECT * FROM person WHERE fornavn = "Erik";
-
-SELECT telefonnummer FROM person WHERE fornavn = "Lise" AND etternavn = "Pise";
-
